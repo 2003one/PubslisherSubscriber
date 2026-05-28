c497e4d0b0cbffb113b7caabf758726725aec775
2003one <2003onede@gmail.com>
2026-05-28 22:45:10 +0530

    initial commit

---
+++ install/pubsub/lib/python3.12/site-packages/pubsub/publisher.py
@@ -0,0 +1,24 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class MyPublisher(Node):
+	def __init__(self):
+		super().__init__('my_publisher')
+		self.publisher = self.create_publisher(String,'chatter',10)
+		self.timer = self.create_timer(1.0,self.publish_message)
+	def publish_message(self):
+		msg = String()
+		msg.data = "Hello Subscriber"
+		self.publisher.publish(msg)
+		self.get_logger().info(f'publishing: {msg.data}')
+
+def main(args = None):
+	rclpy.init(args = args)
+	node = MyPublisher()
+	rclpy.spin(node)
+	rclpy.shutdown()
+
+
+if __name__ == '__main__':
+	main()
---
+++ src/pubsub/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'pubsub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='the2003onean',
+    maintainer_email='2003onede@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+		'pub = pubsub.publisher:main',
+		'sub = pubsub.subscriber:main',
+        ],
+    },
+)
